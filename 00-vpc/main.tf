5c41ea36b818438ee6db7d643cc92de81fa01f54
Blujaytech <blujaytechnlogies@gmail.com>
2026-07-07 20:11:38 +0530

    infra

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,9 +1,5 @@
 module "vpc" {
-<<<<<<< HEAD
-    source = "git::https://github.com/daws-88s/terraform-aws-vpc.git?ref=main"
-=======
     source = "git::https://github.com/HarshaCloud407/Ekart-infra-eks.git?ref=main"
->>>>>>> 52876b2 (infra-building)
     project = var.project
     environment = var.environment
     is_peering_required = true
--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -7,11 +7,7 @@ terraform {
   }
 
   backend "s3" {
-<<<<<<< HEAD
-    bucket  = "blujaytech-devops-demo1" # Replace with your unique bucket name
-=======
     bucket  = "82s-tf-remote-state-dev-407" # Replace with your unique bucket name
->>>>>>> 52876b2 (infra-building)
     key     = "terraform-eks-vpc"
     region  = "us-east-1"
     encrypt = true
