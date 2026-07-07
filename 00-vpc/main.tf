11cf068e65d1a2d6c1fc6ae02dba7d06a8fc5fd9
Blujaytech <blujaytechnlogies@gmail.com>
2026-07-07 21:07:00 +0530

    infra-building

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "git::https://github.com/HarshaCloud407/Ekart-infra-eks.git?ref=main"
+    source = "git::https://github.com/daws-88s/terraform-aws-vpc.git?ref=main"
     project = var.project
     environment = var.environment
     is_peering_required = true
